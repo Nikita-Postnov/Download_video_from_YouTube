673745cfd1283dc9d32ef1648ba1c55551effab9
Nikita Postnov <117923446+NikitaPostnov101@users.noreply.github.com>
2023-07-22 22:02:45 +0300

    Add files via upload

---
+++ Save.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from pytube import YouTube
+
+def download_video():
+    url = entry.get()
+    try:
+        yt = YouTube(url)
+        video_stream = yt.streams.filter(progressive=True, file_extension="mp4").first()
+        video_stream.download()
+        label.config(text="Загрузка успешно завершена!", fg="green")
+    except Exception as e:
+        label.config(text="Произошла ошибка при загрузке видео:", fg="red")
+        print(str(e))
+
+# Создаем окно
+root = tk.Tk()
+root.title("Скачивание видео с YouTube")
+
+# Добавляем элементы интерфейса
+label = tk.Label(root, text="Введите ссылку на видео YouTube:")
+label.pack()
+
+entry = tk.Entry(root, width=50)
+entry.pack()
+
+download_button = tk.Button(root, text="Скачать видео", command=download_video)
+download_button.pack()
+
+root.mainloop()
